58f96e99d2db0ed18f77351530dd17cc63ac8255
Ricard Marxer <rmarxer@mtg110.upf.es>
2008-12-12 17:02:59 +0100

    added the fft using fftw3

---
+++ src/fft.cpp
@@ -0,0 +1,75 @@
+/*                                                         
+** Copyright (C) 2008 Ricard Marxer <email@ricardmarxer.com>
+**                                                                  
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or   
+** (at your option) any later version.                                 
+**                                                                     
+** This program is distributed in the hope that it will be useful,     
+** but WITHOUT ANY WARRANTY; without even the implied warranty of      
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+** GNU General Public License for more details.                        
+**                                                                     
+** You should have received a copy of the GNU General Public License   
+** along with this program; if not, write to the Free Software         
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+*/                                                                          
+
+#include <Eigen/Core>
+#include <Eigen/Array>
+#include <iostream>
+
+#include "fft.h"
+
+#include "debug.h"
+#include "typedefs.h"
+
+using namespace std;
+
+// import most common Eigen types 
+USING_PART_OF_NAMESPACE_EIGEN
+
+FFT::FFT(int frameSize, int fftSize, bool zeroPhase) {
+  DEBUG("FFT: Constructor frameSize: " << frameSize << ", fftSize: " << fftSize << ", zeroPhase: " << zeroPhase);
+
+  _frameSize = frameSize;
+  _fftSize = fftSize;
+  _zeroPhase = zeroPhase;
+
+  DEBUG("FFT: Constructed");
+}
+
+FFT::~FFT(){
+  fftwf_destroy_plan(_fftplan);
+  fftwf_free(_in); 
+  fftwf_free(_out);
+}
+
+void FFT::setup(){
+  DEBUG("FFT: Setting up...");
+  Complex* _input;
+  
+  _in = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * _fftSize);
+  _out = (fftwf_complex*) fftwf_malloc(sizeof(fftwf_complex) * _fftSize);
+  _fftplan = fftwf_plan_dft_1d(_fftSize, _in, _out, FFTW_FORWARD, FFTW_ESTIMATE);
+  
+  DEBUG("FFT: Finished set up...");
+}
+
+void FFT::process(MatrixXC frames, MatrixXC* ffts){
+  for (int i = 0; i < frames.rows(); i++){    
+    // Put the data in _in
+    Eigen::Map<MatrixXC>(reinterpret_cast< Complex* >(_in), 1, _fftSize) = MatrixXR::Zero(1, _fftSize);
+    Eigen::Map<MatrixXC>(reinterpret_cast< Complex* >(_in), 1, _fftSize).block(0, 0, 1, _frameSize) = frames.row(i);
+    
+    // Process the data
+    fftwf_execute(_fftplan);
+
+    // Take the data from _out
+    (*ffts).row(i) = Eigen::Map<MatrixXC>(reinterpret_cast< Complex* >(_out), 1, _fftSize);
+  }
+}
+
+void FFT::reset(){
+}
---
+++ src/fft.h
@@ -0,0 +1,57 @@
+/*                                                         
+** Copyright (C) 2008 Ricard Marxer <email@ricardmarxer.com>
+**                                                                  
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or   
+** (at your option) any later version.                                 
+**                                                                     
+** This program is distributed in the hope that it will be useful,     
+** but WITHOUT ANY WARRANTY; without even the implied warranty of      
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+** GNU General Public License for more details.                        
+**                                                                     
+** You should have received a copy of the GNU General Public License   
+** along with this program; if not, write to the Free Software         
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+*/                                                                          
+
+#ifndef FFT_H
+#define FFT_H
+
+#include <Eigen/Core>
+#include <Eigen/Array>
+#include <iostream>
+
+#include <fftw3.h>
+
+#include "spectralbands.h"
+#include "typedefs.h"
+
+using namespace std;
+
+// import most common Eigen types 
+USING_PART_OF_NAMESPACE_EIGEN
+
+class FFT{
+protected:
+  int _frameSize;
+  int _fftSize;
+  bool _zeroPhase;
+ 
+  fftwf_complex* _in;
+  fftwf_complex* _out;
+
+  fftwf_plan _fftplan;
+
+public:
+  FFT(int frameSize, int fftSize, bool zeroPhase = true);
+  ~FFT();
+  
+  void process(MatrixXC frames, MatrixXC* fft);
+  
+  void setup();
+  void reset();
+};
+
+#endif  /* FFT_H */
