70100c7f2b97aff2ee2ad038a545808de4f633ac
Ricard Marxer <rmarxer@rikrdito.(none)>
2008-12-13 16:36:47 +0100

    more work on plotting the results

--- test_fft.py
+++ test_fft.py
@@ -4,8 +4,8 @@
 import scipy
 import ricaudio
 
-frameSize = 64
-fftSize = 128
+frameSize = 121
+fftSize = 2048
 samplerate = 8000
 
 a_zeros = scipy.array(scipy.zeros((1, frameSize)), dtype='f4')
@@ -37,16 +37,23 @@ print scipy.allclose(r_ones, s_ones)
 print scipy.allclose(r_random, s_random)
 print scipy.allclose(r_sine, s_sine)
 
+r_abs = scipy.absolute(r_sine).T
+r_ang = scipy.angle(r_sine).T
+r_max = max(r_abs)
 
+s_abs = scipy.absolute(s_sine).T
+s_ang = scipy.angle(s_sine).T
+s_max = max(s_abs)
 
 import pylab
-pylab.hold(True)
 pylab.subplot(211)
-pylab.plot(scipy.absolute(r_sine).T, label = 'Ricaudio')
-pylab.plot(scipy.absolute(s_sine), label = 'Scipy')
+pylab.hold(True)
+pylab.plot(r_abs, label = 'Ricaudio')
+pylab.plot(s_abs, label = 'Scipy')
 
 pylab.subplot(212)
-pylab.plot(scipy.angle(r_sine).T, label = 'Ricaudio')
-pylab.plot(scipy.angle(s_sine), label = 'Scipy')
+pylab.hold(True)
+pylab.plot(r_abs*r_ang/r_max, label = 'Ricaudio')
+pylab.plot(s_abs*s_ang/s_max, label = 'Scipy')
 
 pylab.show()
