da6c6bc935ca54e434647be183349979d0bf53c5
Ricard Marxer <email@ricardmarxer.com>
2009-01-20 20:52:28 +0100

    more testing of the odf

--- python/test_odf.py
+++ python/test_odf.py
@@ -8,8 +8,10 @@ a = [[10,  20,  6,  4],
      [10,  100,  20,  0]]
 
 d1 = ricaudio.ODF(8, ricaudio.ODF.COMPLEX_DOMAIN)
-
 d2 = ricaudio.ODF(8, ricaudio.ODF.RECTIFIED_COMPLEX_DOMAIN)
+d3 = ricaudio.ODF(8, ricaudio.ODF.PHASE_DEVIATION)
+d4 = ricaudio.ODF(8, ricaudio.ODF.WEIGHTED_PHASE_DEVIATION)
+d5 = ricaudio.ODF(8, ricaudio.ODF.SPECTRAL_FLUX)
 
 b1 = d1.process(a)
 b2 = d2.process(a)
