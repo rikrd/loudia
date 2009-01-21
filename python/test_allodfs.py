5de7938f1d771da030b5b8b1ccef746a513e221d
Ricard Marxer <email@ricardmarxer.com>
2009-01-21 18:42:10 +0100

    more work on the odfs and got a figure for the docs

---
+++ python/test_allodfs.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import ricaudio
+from sepel.inputs import pyricaudio
+import pylab
+import sys
+import scipy
+
+filename = sys.argv[1]
+
+frameSize = 1024 
+frameStep = 256
+
+frameSizeTime = frameSize / 44100.0
+frameStepTime = frameStep / 44100.0
+
+fftSize = 2048
+
+analysisLimit = 1000.0
+
+# Creation of the pipeline        
+stream = pyricaudio.sndfilereader({'filename': filename,
+                                   'windowSizeInTime': frameSizeTime,
+                                   'windowStepInTime': frameStepTime,
+                                   'encodingKey': 'encoding',
+                                   'channelCountKey': 'channelCount',
+                                   'samplesOriginalKey': 'samples',
+                                   'samplesKey': 'samplesMono',
+                                   'samplerateKey': 'samplerate',
+                                   'timestampBeginKey': 'timestampBegin',
+                                   'timestampEndKey': 'timestampEnd',
+                                   'limit':analysisLimit})
+
+
+stream = pyricaudio.window_ricaudio(stream, {'inputKey': 'samplesMono',
+                                             'outputKey': 'windowed',
+                                             'windowType': 'hamming'})
+
+stream = pyricaudio.fft_ricaudio(stream, {'inputKey': 'windowed',
+                                          'outputKey': 'fft',
+                                          'zeroPhase': True,
+                                          'fftLength': fftSize})
+
+
+odfNames = [(getattr(ricaudio.ODF, i), i) for i in dir(ricaudio.ODF)
+            if type(getattr(ricaudio.ODF, i)) == int]
+
+odfNames.sort()
+
+specs = []
+ffts = []
+odfs = {}
+
+for frame in stream:
+    fft = scipy.array(frame['fft'][:fftSize/2], dtype = scipy.complex64)
+    mag =  scipy.array(abs(fft), dtype = 'f4')
+    spec =  20.0 / scipy.log( 10.0 ) * scipy.log( abs( fft ) + 1e-7)
+
+    ffts.append( fft )
+    specs.append( spec )
+
+ffts = scipy.array( ffts )
+specs = scipy.array( specs )
+
+subplots = len(odfNames) + 1
+
+pylab.figure()
+pylab.hold(True)
+pylab.subplot(subplots, 1, 1)
+pylab.imshow( scipy.flipud(specs.T), aspect = 'auto' )
+pylab.title( 'Spectrogram' )
+
+# Create the ODF processors and process
+for i, (odfType, odfName) in enumerate(odfNames):
+    odf = ricaudio.ODF( fftSize, odfType )
+    odfValues = odf.process( ffts )
+
+    pylab.subplot(subplots, 1, i+2)
+    pylab.plot(odfValues[:,0])
+    pylab.title( odfName.replace('_', ' ').capitalize() )
+        
+pylab.show()
