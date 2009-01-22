3bd7d1c140ee8ee82e4794ebf00972bdee2ac77d
Ricard Marxer <email@ricardmarxer.com>
2009-01-22 19:28:51 +0100

    add the Mel scales test

--- python/test_melscales.py
+++ python/test_melscales.py
@@ -5,6 +5,7 @@ import ricaudio
 
 lowFreq = 133.0
 highFreq = 22050.0
+numPoints = 1000
 
 f_m = lowFreq
 l = ricaudio.linearToMelGreenwood1990( f_m )
@@ -46,4 +47,23 @@ print 'High Mel Bad:', l, 'mels'
 print 'High Freq Calculated Bad', f_c, 'Hz'
 
 print scipy.allclose(f_c, f_m)
+
+
+linearToMels = [(f.replace('linearToMelMatrix', ''), getattr(ricaudio, f))
+                for f in dir(ricaudio) if f.startswith('linearToMelMatrix')]
+                
+melToLinears = [(f.replace('melToLinearMatrix', ''), getattr(ricaudio, f))
+                for f in dir(ricaudio) if f.startswith('melToLinearMatrix')]
+
+freqs = scipy.arange(lowFreq, highFreq, (highFreq - lowFreq) / numPoints, dtype = 'f4')
+
+import pylab
+pylab.figure()
+
+pylab.hold(True)
+for name, scale in linearToMels:
+    pylab.plot(scale(freqs)[0,:]/max(scale(freqs)[0,:]), label = name)
+
+pylab.legend()
+pylab.show()
 
