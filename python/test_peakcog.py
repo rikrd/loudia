fa1cdd10947829dff5efd73f5503beb2c2eef2fe
Ricard Marxer <email@ricardmarxer.com>
2009-01-24 09:59:52 +0100

    fix the peak COG test

--- python/test_peakcog.py
+++ python/test_peakcog.py
@@ -5,10 +5,10 @@ import scipy
 import ricaudio
 
 fundamental = 440.0
-harmonics = 1
+harmonics = 10
 
 frameSize = 1024
-frameStep = 128
+frameStep = 1
 beginSize = frameSize
 signalSize = 2*frameSize
 fftSize = frameSize * (2**2)
@@ -16,16 +16,16 @@ samplerate = 44100
 
 sine = scipy.zeros((signalSize, 1), dtype = 'f4')
 for i in range(harmonics):
-    print scipy.arange(signalSize - beginSize).shape
     sine[beginSize:, 0] += scipy.array(scipy.cos(2 * scipy.pi * i * fundamental * scipy.arange(signalSize - beginSize) / samplerate), dtype='f4')
 
-sinenoise = sine + (scipy.random.random((signalSize, 1)) - 0.5) * 1.0
+sinenoise = scipy.array(sine + (scipy.random.random((signalSize, 1)) - 0.5) * 0.00125, dtype='f4')
 
 # Ricaudio's solution # --------------------------------- #
 bandwidth = 8
+peakCount = 10
 window = ricaudio.Window(frameSize, ricaudio.Window.BLACKMANHARRIS)
 fft = ricaudio.FFT(frameSize, fftSize)
-peaks = ricaudio.PeakDetect(fftSize / 3, bandwidth)
+peaks = ricaudio.PeakDetect(peakCount, bandwidth)
 cogs = ricaudio.PeakCOG(fftSize, bandwidth)
 
 peakCogs = []
@@ -38,10 +38,12 @@ for i in range((signalSize - frameSize)/frameStep):
     r_sine_peakpos, r_sine_peakmag, r_sine_peakphase = peaks.process(r_sine_fft)
     r_sine_peakcogs = cogs.process(r_sine_fft, r_sine_peakpos)
 
-    peakCogs.append(r_sine_peakcogs[0,:])
+    peakCogs.append(r_sine_peakcogs[0,:5])
 # -------------------------------------------------------- #
 
 peakCogs = scipy.array(peakCogs)
+print peakCogs
+
 import pylab
 pylab.figure()
 pylab.plot(peakCogs)
