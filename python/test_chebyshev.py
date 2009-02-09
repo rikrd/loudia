f0ede7fa9b8f22de1a2a04c2977f4f32e94c3e28
Ricard Marxer <email@ricardmarxer.com>
2009-02-09 21:58:12 +0100

    plot the chebyshev

--- python/test_chebyshev.py
+++ python/test_chebyshev.py
@@ -5,14 +5,40 @@ import scipy
 import scipy.signal
 import ricaudio
 
-order = 8
+plot = True
+
+order = 4
 freq = 0.3
 rp = 0.05
 fs = 8000
 
-rc = ricaudio.Chebyshev( 1, order, freq, rp, 8000 )
+rc = ricaudio.Chebyshev( 1, order, freq, rp, fs )
 sc_b, sc_a = scipy.signal.cheby1(order, rp, freq, btype='low', analog=0, output='ba')
 
-print scipy.allclose(sc_b, rc.b().T) and scipy.allclose(sc_a, rc.a().T)
+rc_b = rc.b().T
+rc_a = rc.a().T
+
+print scipy.allclose(sc_b, rc_b) and scipy.allclose(sc_a, rc_a)
+
+if plot:
+    # Create the omega array
+    npoints = 1000
+    w = scipy.arange(-scipy.pi, scipy.pi, 2*scipy.pi/(npoints), dtype = 'f4')
+
+    # Calculate the frequency response
+    d = ricaudio.freqz(rc_b.T, rc_a.T, w)
+
+    import pylab
+
+    pylab.subplot(2,1,1)
+    pylab.plot(w, abs(d[:,0]))
+    pylab.title('Magnitude of the Frequency Response')
+    
+    pylab.subplot(2,1,2)
+    pylab.plot(w, scipy.angle(d[:,0]))
+    pylab.title('Angle of the Frequency Response')
+    
+    pylab.show()
+
 
 
