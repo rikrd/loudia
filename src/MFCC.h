69ceda6bf240e158fab69abb53881888f1ccf7f7
Ricard Marxer <rmarxer@rikrdinho.(none)>
2009-03-14 21:48:22 +0100

    fix the email addresses in the copyright section

--- src/MFCC.h
+++ src/MFCC.h
@@ -1,5 +1,5 @@
 /*                                                         
-** Copyright (C) 2008 Ricard Marxer <email@ricardmarxer.com.com>
+** Copyright (C) 2008 Ricard Marxer <email@ricardmarxer.com>
 **                                                                  
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
