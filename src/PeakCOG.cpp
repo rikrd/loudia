44faa106d6ca1791010ddb41971634cf993f0aef
Ricard Marxer <rmarxer@mtg110.upf.es>
2009-08-19 13:27:10 +0200

    find the valleys around peaks instead of using a hard coded bandwidth

--- src/PeakCOG.cpp
+++ src/PeakCOG.cpp
@@ -1,20 +1,20 @@
-/*                                                         
+/*
 ** Copyright (C) 2008, 2009 Ricard Marxer <email@ricardmarxer.com>
-**                                                                  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 3 of the License, or   
-** (at your option) any later version.                                 
-**                                                                     
-** This program is distributed in the hope that it will be useful,     
-** but WITHOUT ANY WARRANTY; without even the implied warranty of      
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
-** GNU General Public License for more details.                        
-**                                                                     
-** You should have received a copy of the GNU General Public License   
-** along with this program; if not, write to the Free Software         
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-*/                                                                          
+*/
 
 #include "Typedefs.h"
 #include "Debug.h"
@@ -30,9 +30,9 @@ PeakCOG::PeakCOG(int fftLength, int bandwidth) :
   _fftLength(fftLength),
   _bandwidth(bandwidth)
 {
-  
+
   LOUDIA_DEBUG("PEAKCOG: Constructor fftLength: " << _fftLength);
-  
+
   setup();
 }
 
@@ -42,7 +42,7 @@ PeakCOG::~PeakCOG() {}
 void PeakCOG::setup() {
   // Prepare the buffers
   LOUDIA_DEBUG("PEAKCOG: Setting up...");
- 
+
   reset();
 
   LOUDIA_DEBUG("PEAKCOG: Finished set up...");
@@ -55,31 +55,50 @@ void PeakCOG::process(const MatrixXC& fft, const MatrixXR& peakPos, MatrixXR* pe
   const int cols = fft.cols();
   const int halfCols = min((int)ceil(_fftLength / 2.0), cols);
   const int peakCount = peakPos.cols();
-  
+
   LOUDIA_DEBUG("PEAKCOG: fft.shape " << fft.rows() << "," << fft.cols());
   _spectrumAbs2 = fft.block(0, 0, rows, halfCols).cwise().abs2();
   LOUDIA_DEBUG("PEAKCOG: Spectrum resized rows: " << rows << " halfCols: " << halfCols);
-    
+
   unwrap(fft.block(0, 0, rows, halfCols).cwise().angle(), &_spectrumArg);
   derivate(_spectrumArg, &_spectrumArgDeriv);
-  
+
+  derivate(_spectrumAbs2, &_spectrumAbs2Deriv);
+
   (*peakCog).resize(rows, peakCount);
   (*peakCog).setZero();
 
-  for(int row = 0; row < rows; row++) {
-    for(int i = 0; i < peakCount; i++){     
-      if (peakPos(row, i) != -1) {
-        int start = max(0, (int)floor(peakPos(row, i) - _bandwidth / 2));
-        int end = min(halfCols, (int)ceil(peakPos(row, i) + _bandwidth / 2));
+  for (int row = 0; row < rows; row++) {
+    for (int i = 0; i < peakCount; i++) {
+      if (peakPos(row, i) == -1) {
+        continue;
+      }
 
-        if ( (end - start) > 0) {
-          (*peakCog)(row, i) = ((-_spectrumArgDeriv).block(row, start, 1, end-start).cwise() * _spectrumAbs2.block(row, start, 1, end-start)).sum() / _spectrumAbs2.block(row, start, 1, end-start).sum();
+      // Find the start and end of the peak by finding valleys
+      int start = peakPos(row, i);
+      for (int j = start; j > 0; j-- ) {
+        if (_spectrumAbs2Deriv(row, j) * _spectrumAbs2Deriv(row, j-1) < -2) {
+          start = j;
+          break;
         }
       }
-      
+
+      int end = peakPos(row, i);
+      for (int j = end; j < _spectrumAbs2Deriv.cols()-1; j++ ) {
+        if (_spectrumAbs2Deriv(row, j) * _spectrumAbs2Deriv(row, j+1) < -2) {
+          end = j;
+          break;
+        }
+      }
+
+
+      if ( (end - start) > 0) {
+        (*peakCog)(row, i) = ((-_spectrumArgDeriv).block(row, start, 1, end-start).cwise() * _spectrumAbs2.block(row, start, 1, end-start)).sum() / _spectrumAbs2.block(row, start, 1, end-start).sum();
+      }
+
     }
   }
-  
+
   LOUDIA_DEBUG("PEAKCOG: Finished Processing");
 }
 
--- src/SpectralODFCOG.h
+++ src/SpectralODFCOG.h
@@ -1,20 +1,20 @@
-/*                                                         
+/*
 ** Copyright (C) 2008, 2009 Ricard Marxer <email@ricardmarxer.com>
-**                                                                  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 3 of the License, or   
-** (at your option) any later version.                                 
-**                                                                     
-** This program is distributed in the hope that it will be useful,     
-** but WITHOUT ANY WARRANTY; without even the implied warranty of      
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
-** GNU General Public License for more details.                        
-**                                                                     
-** You should have received a copy of the GNU General Public License   
-** along with this program; if not, write to the Free Software         
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-*/                                                                          
+*/
 
 #ifndef ODFCOG_H
 #define ODFCOG_H
@@ -32,7 +32,7 @@ protected:
   int _fftSize;
   int _peakCount;
   int _bandwidth;
-  
+
   // Internal variables
   MatrixXR _spectrumAbs2;
   MatrixXR _spectrumArg;
@@ -45,9 +45,9 @@ protected:
 
   PeakDetection _peaker;
   PeakCOG _peakCoger;
-  
+
 public:
-  SpectralODFCOG(int fftSize, int bandwidth = 6, int peakCount = 40);
+  SpectralODFCOG(int fftSize, int bandwidth = 1, int peakCount = -1);
 
   ~SpectralODFCOG();
 
