a44ef21c47d1e686592d7e2b0c4db773b59a710b
Ricard Marxer <rmarxer@mtg110.upf.es>
2009-08-19 15:30:51 +0200

    conditonally decide what to include for ffmpeg

--- src/AudioLoader.h
+++ src/AudioLoader.h
@@ -1,20 +1,20 @@
-/*                                                         
+/*
 ** Copyright (C) 2008, 2009 Ricard Marxer <email@ricardmarxer.com>
-**                                                                  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 3 of the License, or   
-** (at your option) any later version.                                 
-**                                                                     
-** This program is distributed in the hope that it will be useful,     
-** but WITHOUT ANY WARRANTY; without even the implied warranty of      
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
-** GNU General Public License for more details.                        
-**                                                                     
-** You should have received a copy of the GNU General Public License   
-** along with this program; if not, write to the Free Software         
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-*/                                                                          
+*/
 
 #ifndef AUDIOLOADER_H
 #define AUDIOLOADER_H
@@ -23,12 +23,19 @@
 #include "Debug.h"
 #include <string>
 
+
 extern "C" {
 #ifndef __STDC_CONSTANT_MACROS
 #define __STDC_CONSTANT_MACROS
 #endif
+
+#ifdef LOUDIA_OLD_FFMPEG
+#include <ffmpeg/avcodec.h>
+#include <ffmpeg/avformat.h>
+#else
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#endif
 }
 
 typedef int16_t sample_type;
@@ -38,16 +45,16 @@ public:
   /**
      @enum Channel
      @brief Specifies the special channels such as ALL and MIX.
-     
+
      @sa channel
   */
-  enum Channel {    
+  enum Channel {
     ALL                   = -1 /**< All the cahnnels available */,
     MONOMIX               = -2 /**< Mono mix of all the channels */,
     LEFT                  =  0 /**< First channel in the stream (left) */,
     RIGHT                 =  1 /**< Second channel in the stream (right) */
   };
-  
+
 private:
   const int _inputFrameSize;
   int _frameSize;
@@ -61,7 +68,7 @@ private:
 
   int _sampleRate;
   int _channelCount;
-  
+
   sample_type *_buffer;
 
   int _bufferSize;
@@ -78,11 +85,11 @@ private:
 
   int64_t _sizeRead;
   int64_t _currentTime;
-  
+
   Real _loadDuration;
   int64_t _loadDurationInTimeBase;
   int64_t _loadedDuration;
-  
+
   void loadFile();
   void closeFile();
 
@@ -120,7 +127,7 @@ public:
   int frameSize() const { return _frameSize; };
 
   void setChannel( int channel, const bool callSetup = true ) { _channel = channel; if ( callSetup ) setup(); };
-  int channel() const { return _channel; };  
+  int channel() const { return _channel; };
 
   void setLoadDuration( Real duration, const bool callSetup = true ) { _loadDuration = duration; if ( callSetup ) setup(); };
   Real loadDuration() const { return _loadDuration; };
@@ -131,7 +138,7 @@ public:
   Real totalTime() const;
 
   void seek( Real time );
-  
+
   bool finished() const { return _finished; };
 };
 
