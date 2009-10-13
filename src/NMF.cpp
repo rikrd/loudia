cd004f7813af841c8469decc95469761f40d75bb
Ricard Marxer <rmarxer@mtg110.upf.es>
2009-10-13 14:05:25 +0200

    port to Eigen trunk

--- src/NMF.cpp
+++ src/NMF.cpp
@@ -1,20 +1,20 @@
-/*                                                         
+/*
 ** Copyright (C) 2008, 2009 Ricard Marxer <email@ricardmarxer.com>
-**                                                                  
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 3 of the License, or   
-** (at your option) any later version.                                 
-**                                                                     
-** This program is distributed in the hope that it will be useful,     
-** but WITHOUT ANY WARRANTY; without even the implied warranty of      
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
-** GNU General Public License for more details.                        
-**                                                                     
-** You should have received a copy of the GNU General Public License   
-** along with this program; if not, write to the Free Software         
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-*/                                                                          
+*/
 
 #include "Typedefs.h"
 #include "Debug.h"
@@ -28,11 +28,11 @@ using namespace Eigen;
 
 NMF::NMF(int inputSize, int componentCount, int iterationCount, Real epsilon)
 {
-  
+
   LOUDIA_DEBUG("NMF: Constructor inputSize: " << inputSize
         << " componentCount: " << componentCount
         << " iterationCount: " << iterationCount );
-  
+
   setInputSize( inputSize, false );
   setComponentCount( componentCount, false );
   setIterationCount( iterationCount, false );
@@ -47,7 +47,7 @@ NMF::~NMF() {}
 void NMF::setup() {
   // Prepare the buffers
   LOUDIA_DEBUG("NMF: Setting up...");
-  
+
   reset();
 
   LOUDIA_DEBUG("NMF: Finished set up...");
@@ -58,16 +58,16 @@ void NMF::process(const MatrixXR& v, MatrixXR* w, MatrixXR* h) {
   LOUDIA_DEBUG("NMF: Processing ...");
   const int rows = v.rows();
   const int cols = v.cols();
-  
+
   // The X matrix is v.transpose()
   // Some beleive it can be useful to normalize
-  
+
   // The W matrix is (*w).transpose()
   (*w).resize(_componentCount, cols);
-  
+
   // The H matrix is (*h).transpose()
   (*h).resize(rows, _componentCount);
-  
+
   // Initializing W and H
   // TODO: initialize with a Normal distribution
   (*w).setRandom();
@@ -75,22 +75,22 @@ void NMF::process(const MatrixXR& v, MatrixXR* w, MatrixXR* h) {
 
   (*h).setRandom();
   (*h) = (*h).cwise().abs();
-  
+
   for (int iter = 0; iter < _iterationCount; iter ++) {
-    _xOverWH = v.transpose().cwise() / (((*w).transpose() * (*h).transpose()).cwise() + _epsilon );
-    
+    _xOverWH = v.transpose().cwise() / ((w->transpose() * h->transpose()).cwise() + _epsilon );
+
     // Multiplicative update rules of W and H by (Lee and Seung 2001)
-    (*w).transpose().cwise() *= (_xOverWH * (*h)).cwise() / (MatrixXR::Ones(cols, 1) * (*h).colwise().sum());
-    
-    (*h).transpose().cwise() *= ((*w) * _xOverWH).cwise() / ((*w).transpose().colwise().sum().transpose() * MatrixXR::Ones(1, rows));
+    VectorXR hColSum = (*h).colwise().sum();
+    (*w).transpose().cwise() *= (_xOverWH * (*h)).cwise() / ColXR::Ones(cols) * hColSum;
+    (*h).transpose().cwise() *= ((*w) * _xOverWH).cwise() / (w->transpose().colwise().sum().transpose() * MatrixXR::Ones(1, rows));
 
     // Renormalize so rows of H have constant energy
     _norms = (*h).colwise().norm();
-    
+
     (*w).transpose().cwise() *= MatrixXR::Ones(cols, 1) * _norms;
     (*h).transpose().cwise() /= _norms.transpose() * MatrixXR::Ones(1, rows);
   }
-  
+
   LOUDIA_DEBUG("NMF: Finished Processing");
 }
 
@@ -101,7 +101,7 @@ void NMF::reset() {
 int NMF::inputSize() const {
   return _inputSize;
 }
-  
+
 void NMF::setInputSize( int size, bool callSetup ) {
   _inputSize = size;
   if ( callSetup ) setup();
@@ -110,7 +110,7 @@ void NMF::setInputSize( int size, bool callSetup ) {
 int NMF::componentCount() const {
   return _componentCount;
 }
-  
+
 void NMF::setComponentCount( int count, bool callSetup ) {
   _componentCount = count;
   if ( callSetup ) setup();
@@ -119,7 +119,7 @@ void NMF::setComponentCount( int count, bool callSetup ) {
 int NMF::iterationCount() const {
   return _iterationCount;
 }
-  
+
 void NMF::setIterationCount( int count, bool callSetup ) {
   _iterationCount = count;
   if ( callSetup ) setup();
@@ -128,7 +128,7 @@ void NMF::setIterationCount( int count, bool callSetup ) {
 Real NMF::epsilon() const {
   return _epsilon;
 }
-  
+
 void NMF::setEpsilon( Real epsilon, bool callSetup ) {
   _epsilon = epsilon;
   if ( callSetup ) setup();
